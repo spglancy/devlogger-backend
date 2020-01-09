e4e5389d3e342a06ee1523132da72f23c31a994a
Sean Glancy <spglancy@gmail.com>
2020-01-09 11:24:14 -0800

    heroku update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json())
 
 // Setting application routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'https://master.d16ie1zeeyafc2.amplifyapp.com/')
   res.header('Access-Control-Allow-Methods', 'POST GET')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
